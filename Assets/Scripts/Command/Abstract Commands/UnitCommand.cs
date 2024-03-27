9375d89a30ad3a5d3564c98015677f63311d984e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-27 11:30:06 +0530

    Put all ids in a structure

---
+++ Assets/Scripts/Command/Abstract Commands/UnitCommand.cs
@@ -0,0 +1,32 @@
+using Command.Player;
+
+namespace Command.Commands
+{
+    public struct CommandData
+    {
+        public int ActorUnitID;
+        public int TargetUnitID;
+        public int ActorPlayerID;
+        public int TargetPlayerID;
+
+        public CommandData(int ActorUnitID, int TargetUnitID, int ActorPlayerID, int TargetPlayerID)
+        {
+            this.ActorUnitID = ActorUnitID;
+            this.TargetUnitID = TargetUnitID;
+            this.ActorPlayerID = ActorPlayerID;
+            this.TargetPlayerID = TargetPlayerID;
+        }
+    }
+    
+    public abstract class UnitCommand : ICommand
+    {
+        public CommandData commandData;
+
+        protected UnitController actorUnit;
+        protected UnitController targetUnit;
+
+        public abstract void Execute();
+
+        public abstract bool WillHitTarget();
+    }
+}
