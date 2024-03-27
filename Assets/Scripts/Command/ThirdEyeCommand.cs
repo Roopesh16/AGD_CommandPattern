b43b841163b7d0a8f567e2303df200a0464677f6
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-27 12:48:11 +0530

    Add third eye command

---
+++ Assets/Scripts/Command/ThirdEyeCommand.cs
@@ -0,0 +1,21 @@
+using Command.Actions;
+using Command.Main;
+
+namespace Command.Commands
+{
+    public class ThirdEyeCommand : UnitCommand
+    {
+        private bool willHitTarget;
+
+        public ThirdEyeCommand(CommandData commandData)
+        {
+            this.commandData = commandData;
+            willHitTarget = WillHitTarget();
+        }
+
+        public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.ThirdEye)
+            .PerformAction(actorUnit, targetUnit, willHitTarget);
+
+        public override bool WillHitTarget() => true;
+    }
+}
