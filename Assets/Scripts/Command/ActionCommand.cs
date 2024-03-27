a0711fc57b4588d44819fcdbc7f206afa817a51e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-27 12:34:47 +0530

    Add action concrete command

---
+++ Assets/Scripts/Command/ActionCommand.cs
@@ -0,0 +1,21 @@
+using Command.Actions;
+using Command.Main;
+
+namespace Command.Commands
+{
+    public class ActionCommand : UnitCommand
+    {
+        private bool willHitTarget;
+
+        public ActionCommand(CommandData commandData)
+        {
+            this.commandData = commandData;
+            willHitTarget = WillHitTarget();
+        }
+
+        public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Attack)
+            .PerformAction(actorUnit, targetUnit, willHitTarget);
+
+        public override bool WillHitTarget() => true;
+    }
+}
