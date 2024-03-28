138957caed481225e11fb6b4cecad1a28a0ebae2
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-28 13:25:01 +0530

    Undo in Berserk command

--- Assets/Scripts/Command/ConcreteCommands/BerserkAttackCommand.cs
+++ Assets/Scripts/Command/ConcreteCommands/BerserkAttackCommand.cs
@@ -15,7 +15,19 @@ namespace Command.Commands
         }
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.BerserkAttack).PerformAction(actorUnit, targetUnit, willHitTarget);
-
+        
+        public override void Undo()
+        {
+            if (willHitTarget)
+            {
+                if(!targetUnit.IsAlive())
+                    targetUnit.Revive();
+                
+                targetUnit.RestoreHealth(actorUnit.CurrentPower);
+                actorUnit.Owner.ResetCurrentActiveUnit();
+            }
+        }
+        
         public override bool WillHitTarget() => Random.Range(0f, 1f) < hitChance;
     }
 }
