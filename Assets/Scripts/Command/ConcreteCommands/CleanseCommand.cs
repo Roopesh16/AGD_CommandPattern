761aa15979b4c2f5ee1294cfd801db9b1126027f
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-28 12:58:07 +0530

    Add undo to cleanse command

--- Assets/Scripts/Command/ConcreteCommands/CleanseCommand.cs
+++ Assets/Scripts/Command/ConcreteCommands/CleanseCommand.cs
@@ -16,6 +16,18 @@ namespace Command.Commands
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Cleanse).PerformAction(actorUnit, targetUnit, willHitTarget);
 
+        public override void Undo()
+        {
+            if (willHitTarget)
+            {
+                if (!targetUnit.IsAlive())
+                    targetUnit.Revive();
+
+                targetUnit.RestoreHealth(actorUnit.CurrentPower);
+                actorUnit.Owner.ResetCurrentActiveUnit();
+            }
+        }
+
         public override bool WillHitTarget() => Random.Range(0f, 1f) < hitChance;
     }
 }
