772e6ffdca1abcc9042afccc44adce2c41dc15fe
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-28 12:41:19 +0530

    Add undo in heal command

--- Assets/Scripts/Command/ConcreteCommands/HealCommand.cs
+++ Assets/Scripts/Command/ConcreteCommands/HealCommand.cs
@@ -13,6 +13,12 @@ namespace Command.Commands
         }
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Heal).PerformAction(actorUnit, targetUnit, willHitTarget);
+        
+        public override void Undo()
+        {
+            targetUnit.TakeDamage(actorUnit.CurrentPower);
+            actorUnit.Owner.ResetCurrentActiveUnit();
+        }
 
         public override bool WillHitTarget() => true;
     }
